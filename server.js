f0c3d66bcc39dae22dc39ef1c428827fe780a7cc
Aswin M S <aswinms200111@gmail.com>
2025-04-21 15:16:01 +0530

    commit before admin route and login,and database cloude are not added

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+import path from "path";
 import dotenv from "dotenv";
 import express from 'express';
 import mongoose from "mongoose";
+import { fileURLToPath } from "url";
 dotenv.config();
 const app = express();
 
@@ -9,10 +11,12 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 app.get ('/', (req, res) => {
-  res.send ('Hello World!');
+  res.sendFile(path.join(__dirname, "public","images","Home.png"))
 }
 );
 
