78ae3511299ffc2e54d5dba0c9592853047cb31f
Aswin M S <aswinms200111@gmail.com>
2025-10-06 10:36:48 +0530

    commit after changing google auth

--- config/passport.js
+++ config/passport.js
@@ -41,7 +41,7 @@ passport.use(new LocalStrategy(
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://www.aromixx.shop/users/google/callback",
+    callbackURL: "https://aromixx.shop/users/google/callback",
 
 
 },async(accessToken,refreshToken,profile,done)=>{
